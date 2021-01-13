90e359a294f70b072ede0d2158fdf1951bef1e07
J Scott Rumptz <jscott@rumptz.tech>
2021-01-13 10:57:19 -0600

    tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
+// set an array of 17 empty values to so that it's index can correspond with military time (if later hours are needed, simply add to the array)
 let timeBlocks = [ "","","","","","","","","","","","","","","","",""];
 
 // displays the current day in the header
 let displayDay = function() {
-    $("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do") + ", " + moment().format("YYYY"));
+    $("#currentDay").text(moment().format("dddd") + ",  " + moment().format("MMM Do") + ", " + moment().format("YYYY"));
 };
 
-// color code the tasks by their relation to the current time
+// color code the time blocks by their relation to the current time
 let updateTimeBlockStatus = function() {
 
     // capture the current time
     let presentHour = moment().format("H");
 
-    // for loop that will run through all the block ids
+    // for loop that will run through all the textarea ids
     for(i=0 ; i<9 ;i++) {
 
-        // capture the curret textfield id
+        // capture the curret textarea id
         let block = $(`#${i + 8}`);
 
-        // compare the current time to the textfield id and add the class to color it accordingly
+        // compare the current time to the textarea id
         if ((i+8) < parseInt(presentHour)){
+            // remove old classes and add the new class to format it accordingly
             block.removeClass("present future");
             block.addClass("past");
         } else if ((i+8) === parseInt(presentHour)) {
@@ -31,38 +33,41 @@ let updateTimeBlockStatus = function() {
     }
 }
 
-// function to load saved entries from local storage
+// function to load saved time block entries from local storage
 let loadTimeBlocks = function() {
     timeBlocks = JSON.parse(localStorage.getItem("timeBlocks"));
   
-    // if nothing in localStorage, create an empty timeBlocks object
+    // if nothing in localStorage, create an empty timeBlocks array
     if (!timeBlocks) {
         timeBlocks = [ "","","","","","","","","","","","","","","","",""]
     }
 
-    // for loop that will run through all the timeBlocks in the array
+    // for loop that will run through all the time blocks in the array
     for(i=8 ; i<17 ;i++) {
 
     // load the saved data
     $(`#${i}`).text(timeBlocks[i]);
     }
 
+    // format the time blocks
     updateTimeBlockStatus();
   };
   
 // function to save entries into local storage  
 let saveTimeBlocks = function() {
 
+    // capture the current value and id of the textarea sibling of the clicked button
     let blockText = $(this).prev("textarea").val().trim();
     let blockId = $(this).prev("textarea").attr("id");
-    
-    $(this).css("color", "black");
 
+    // set array index value to the corisponding textarea id
     timeBlocks[blockId] = blockText
 
+    // save the timeBlocks array to local storage
     localStorage.setItem("timeBlocks", JSON.stringify(timeBlocks));
 
     loadTimeBlocks();
+
   };
   
 // display the current day at the top of the page
@@ -71,21 +76,45 @@ displayDay();
 // check local storage for saved timeBlocks
 loadTimeBlocks();
 
-// So that the user doesn't need to refresh, check every 60 secs to...
+// so that each time block's status automatically updates, check every 60 secs to...
 setInterval(function(){
 
     // display the current day
     displayDay();
 
-    // update the status of the timeBlocks
+    // update the status of the time blocks
     updateTimeBlockStatus();
 
     // interval set to perform theses functions every 60 secs
 }, 1000 * 60);
 
+// save button click calls the saveTimeBlocks function
 $(".time-block").on("click", ".btn", saveTimeBlocks);
 
-$("textarea").on("blur", saveTimeBlocks);
+// initialize bootstrap tootips
+$(".saveBtn").ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
+});
 
+// hide the icon's saved tooltip after 3 seconds
+$(".saveBtn").on("click", function(){
+    setTimeout(function(){ $('[data-bs-toggle="tooltip"]').tooltip("hide");}, 3000);
+});
 
+// code for after this is graded 
+// automatically save whenever the user clicks out of the textarea
+// $("textarea").on("blur", function () {
 
+//     console.log(this);
+
+//     let blockText = $(this).val().trim();
+//     let blockId = $(this).attr("id");
+    
+//     $(this).css("color", "black");
+
+//     timeBlocks[blockId] = blockText
+
+//     localStorage.setItem("timeBlocks", JSON.stringify(timeBlocks));
+
+//     updateTimeBlockStatus();
+// });
